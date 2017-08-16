09140031d39b509955b975464c4352b5bcb29d75
kapoorh <harsh.kapoor@birlasoft.com>
2017-08-16 16:21:35 +0530

    commit

--- StudentCRUD/Controllers/HomeController.cs
+++ StudentCRUD/Controllers/HomeController.cs
@@ -136,22 +136,22 @@ namespace StudentCRUD.Controllers
 
             catch(Exception exp)
             {
-                
+                throw;
             }
                 
         }
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
     }
 }
